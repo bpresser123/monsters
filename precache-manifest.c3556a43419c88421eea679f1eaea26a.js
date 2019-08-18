b30ef555ba9a6474268aba91e5ed5fdd9c3d9b31
Brian Presser <bmpresser@gmail.com>
2019-08-18 17:19:11 -0400

    Updates

---
+++ precache-manifest.c3556a43419c88421eea679f1eaea26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "815d7b5c7a6d1b02f527cd5e44ee79cc",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "c6318b3c1b2c2bf31161",
+    "url": "/monsters/static/css/main.3c4911e3.chunk.css"
+  },
+  {
+    "revision": "1385b2595d264d337bf2",
+    "url": "/monsters/static/js/2.c974ffbb.chunk.js"
+  },
+  {
+    "revision": "c6318b3c1b2c2bf31161",
+    "url": "/monsters/static/js/main.3c7b1690.chunk.js"
+  },
+  {
+    "revision": "794baf5fe06c6b3192b0",
+    "url": "/monsters/static/js/runtime~main.b6c3df64.js"
+  }
+]);
